24ea6dbfc9a7715dd9f4cf70f896b07ef6c4d9c3
Thanh Lâm Nguyễn <lamnguyen11e@gmail.com>
2020-06-03 11:20:14 +0700

    create navbar frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import Homepage from './Components/Homepage/homepage'
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/Components/Homepage/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div>
+            <nav className="nav navbar-dark bg-dark ">
+                <a className="navbar-brand ml-3 font-weight-bold" href="/">1612324 - EthWallet</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Homepage
